d5f85ef5f8292e330e779e1690e806fd7658373f
W.Bielecki <bva@sibdigital.net>
2022-04-06 22:32:04 +0800

    v2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,47 @@
 import java.io.File
+import java.io.FileFilter
+import java.nio.file.FileSystem
 
-var commands = mutableListOf<Command>()
-
+val ENDLINE = "\r\n"
+var path = "/home"
+var filemask = "*.txt"
+var fileList = mutableListOf<String>()
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("======================= Find files =========================")
+    println("=======================  v.2.0.0   =========================")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-    ClearArgs(args)
-    commands.Compile()
-
-}
-
-private fun MutableList<Command>.Compile() {
-    forEach { it ->
-        if(it.command == CommandType.file){
-            val path = it.param
-            File(path).walk().forEach {
-                println(it.absoluteFile)
+    // найти файл параметров программы
+    val file:File = File("params.config")
+    if(!file.exists()){
+        println("${file} - not found")
+        file.createNewFile()
+        println("Create new file ${file.absolutePath}")
+        file.writeText("path="+path+ENDLINE+"filemask="+filemask)
+    }
+    if(!file.canRead()){
+        println("Can`t read file. Close")
+        return
+    }else{
+        println("Read file ${file.absoluteFile}")
+        file.readLines().forEach {
+            val line = it.toString()
+            if(line.split("=").get(0).equals("path",true)){
+                path = line.split("=").get(1)
+            }
+            if(line.split("=").get(0).equals("filemask",true)){
+                filemask = line.split("=").get(1)
             }
         }
     }
+    //output list of files
+
+
+
+
+
 }
 
 
 
-
-fun ClearArgs(args: Array<String>) {
-    //выбираем только валидные параметры
-    var clearArr = args.filter { item->valid(item) }
-    for (item in clearArr){
-       var comm = item.split("=").get(0).get(1)
-       var param = item.split("=").get(1)
-       val command = Command()
-       command.command = if(comm == 'p') CommandType.file else CommandType.none
-       command.param = param
-       commands.add(command)
-    }
-}
-
-fun valid(item: String):Boolean {
-    val r1 = Regex("""^-[fp]=.+$""",RegexOption.IGNORE_CASE)
-    return r1.matches(item)
-}
 
 
